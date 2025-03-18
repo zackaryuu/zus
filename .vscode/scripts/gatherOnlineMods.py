8ebe1a97017c9d15a17da76d355d6f5d2bd006fc
ZackaryW <gitzackw@gmail.com>
2025-03-18 11:06:05 -0700

    ci: record install methods

--- .vscode/scripts/gatherOnlineMods.py
+++ .vscode/scripts/gatherOnlineMods.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         print(f"Failed to fetch repositories: {response.status_code}")
         exit(1)
 
-    eligibles = []
+    eligibles = {}
     repos = response.json()
     for repo in repos:
         # filter
@@ -18,7 +18,11 @@ if __name__ == "__main__":
         if repo["name"].startswith("z") or repo["name"].startswith("old"):
             continue
 
-        eligibles.append(repo["name"])
+        topics = repo["topics"]
+        if "no-update" in topics:
+            eligibles[repo["name"]] = False
+        else:
+            eligibles[repo["name"]] = True
 
     with open("src/zs/mods/orgRepos.json", "w") as f:
         json.dump(eligibles, f)
