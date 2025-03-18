110e74b0d32b62580c3c286d32f5be99e4214e84
ZackaryW <gitzackw@gmail.com>
2025-03-18 11:41:08 -0700

    feat(zs): shell impl

--- src/zs/common/zs.py
+++ src/zs/common/zs.py
@@ -1,12 +1,35 @@
-
-
+import json
+from functools import cache
 import os
 from zs.mods import MOD_PATH
+
+SHIM_PATH = os.path.join(os.path.expanduser("~"), ".zs", "shims")
+INSTALLED_PATH = os.path.join(os.path.expanduser("~"), ".zs", "installed")
+
+os.makedirs(INSTALLED_PATH, exist_ok=True)
+os.makedirs(SHIM_PATH, exist_ok=True)
 
 def isBuiltinPkg(name : str)->bool:
     if not name.endswith(".py"):
         name = f"{name}.py"
     return os.path.exists(os.path.join(MOD_PATH, name))
     
-INSTALLED_PATH = os.path.join(os.path.expanduser("~"), ".zs", "installed")
 
+@cache
+def listInstalled()->list[str]:
+    installed = []
+    for pkg in os.listdir(SHIM_PATH):
+        if pkg.startswith(".") or pkg.startswith("_"):
+            continue
+
+        pkg = pkg.replace(".exe", "")
+        pkg = pkg.replace("zs", "")
+        installed.append(pkg)
+    return installed
+
+@cache
+def listIndex()-> dict[str, bool]:
+    with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "mods", "orgRepos.json"), "r") as f:
+        data = json.load(f)
+
+    return data
--- src/zs/mods/zs.py
+++ src/zs/mods/zs.py
@@ -1,37 +1,121 @@
+import shutil
 import click
 import os
-from zs.common.zs import INSTALLED_PATH, isBuiltinPkg
-from zs.mods import MOD_PATH
+import json
+import zs.common.zs as zs
+import tempfile
+
+PS2EXE_SCRIPT = """
+#!/usr/bin/env pwsh
+$cliPath = "{cli_path}"
+python $cliPath @args
+exit $LASTEXITCODE
+"""
 
 @click.group()
 def cli():
     pass
 
+@cli.command()
+@click.option("--giturl", "-g", help="Git URL")
+def install(name : str, giturl : str):
+    if giturl or name not in zs.listIndex():
+        click.echo(f"zs.install: {name} not found")
+        return
+
+    if name in zs.listInstalled():
+        click.echo(f"zs.install: {name} already installed")
+        return
+    
+    if not giturl:
+        giturl = f"https://github.com/zackaryuu/{name}.git"
+
+    # git clone at INSTALLED_PATH
+    os.system(f"git clone {giturl} {zs.INSTALLED_PATH}/{name}")
+
+    cli_path = os.path.join(zs.INSTALLED_PATH, name, "cli.py")
+    if not os.path.exists(cli_path):
+        if os.path.exists(os.path.join(zs.INSTALLED_PATH, name, "src", "cli.py")):
+            cli_path = os.path.join(zs.INSTALLED_PATH, name, "src", "cli.py")
+        else:
+            click.echo(f"zs.install: {name} not found")
+            return
+
+    # if no update
+    if name in zs.listIndex() and zs.listIndex()[name]:
+        # remove .git
+        os.system(f"rm -rf {zs.INSTALLED_PATH}/{name}/.git")
+
+    # create ps2exe script
+    with tempfile.NamedTemporaryFile(delete=False) as f:
+        f.write(PS2EXE_SCRIPT.format(cli_path=cli_path).encode())
+        f.flush()
+        os.system(f"ps2exe -inputFile {f.name} -outputFile {os.path.join(zs.SHIM_PATH, f'zs.{name}.exe')}")
+
 
 @cli.command()
-@click.option("--builtin", "-b", is_flag=True, help="List builtin packages")
-@click.option("--installed", "-i", is_flag=True, help="List installed packages")
-def list(builtin, installed):
-    both = not builtin and not installed
+@click.argument("name", nargs=-1)
+def update(name : list[str]):
+    if not name:
+        name = zs.listInstalled()
 
-    if builtin:
-        if not both:
-            click.echo("Builtin packages:")
-        for pkg in os.listdir(MOD_PATH):
-            if isBuiltinPkg(pkg):
-                if not both:
-                    click.echo(pkg)
-                else:
-                    click.echo(f"{pkg} (builtin)")
+    for n in name:
+        if n not in zs.listInstalled():
+            click.echo(f"zs.update: {n} not installed")
+            continue
 
-    if installed:
-        if not both:
-            click.echo("Installed packages:")
-        for pkg in os.listdir(INSTALLED_PATH):
-            if not both:
-                click.echo(pkg)
-            else:
-                click.echo(f"{pkg} (installed)")
+        if not os.path.exists(os.path.join(zs.INSTALLED_PATH, n, ".git")):
+            click.echo(f"zs.update: {n} not a git repository")
+            continue
+
+        # git pull
+        os.system(f"git pull {zs.INSTALLED_PATH}/{n}")
+
+@cli.command()
+@click.argument("name", nargs=-1)
+def remove(name : list[str]):
+    if not name:
+        click.echo("zs.remove: No name provided")
+        return
+
+    for n in name:
+        if n not in zs.listInstalled():
+            click.echo(f"zs.remove: {n} not installed")
+            continue
+
+        shutil.rmtree(os.path.join(zs.INSTALLED_PATH, n))
+
+        os.remove(os.path.join(zs.SHIM_PATH, f"zs.{n}.exe"))
+
+        click.echo(f"zs.remove: {n} removed")
+
+@cli.group()
+def list():
+    pass
+
+@list.command()
+def builtin():
+    for pkg in os.listdir(zs.MOD_PATH):
+        if zs.isBuiltinPkg(pkg):
+            pkg = pkg.replace(".py", "")
+            click.echo(pkg)
+
+@list.command()
+def installed():
+    for pkg in zs.listInstalled():
+        click.echo(pkg)
+
+@list.command()
+def index():
+    path = os.path.join(os.path.dirname(__file__), "orgRepos.json")
+    with open(path, "r") as f:
+        data = json.load(f)
+
+    for pkg in data:
+        if pkg in zs.listInstalled():
+            click.echo(f"{pkg} (installed)")
+        else:
+            click.echo(pkg)
 
 if __name__ == "__main__":
     cli()
